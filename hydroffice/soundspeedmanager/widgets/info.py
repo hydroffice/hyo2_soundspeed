33d806860d73d1942b93cb3051b796126b0e9482
giumas <giumas@yahoo.it>
2016-10-05 21:06:25 +0000

    added authors widget

--- hydroffice/soundspeedmanager/widgets/info.py
+++ hydroffice/soundspeedmanager/widgets/info.py
@@ -36,11 +36,24 @@ class Info(QtGui.QMainWindow):
         license_action.setShortcut('Alt+L')
         license_action.triggered.connect(self.load_license)
         self.toolbar.addAction(license_action)
+        # authors
+        authors_action = QtGui.QAction(QtGui.QIcon(os.path.join(self.media, 'authors.png')), 'Authors', self)
+        authors_action.setShortcut('Alt+A')
+        authors_action.triggered.connect(self.show_authors)
+        self.toolbar.addAction(authors_action)
+
+        # separator
+        self.toolbar.addSeparator()
         # HydrOffice.org
         hyo_action = QtGui.QAction(QtGui.QIcon(os.path.join(self.media, 'hyo.png')), 'HydrOffice.org', self)
         hyo_action.setShortcut('Ctrl+H')
         hyo_action.triggered.connect(self.load_hydroffice_org)
         self.toolbar.addAction(hyo_action)
+        # noaa
+        noaa_action = QtGui.QAction(QtGui.QIcon(os.path.join(self.media, 'noaa.png')), 'nauticalcharts.noaa.gov', self)
+        noaa_action.setShortcut('Alt+N')
+        noaa_action.triggered.connect(self.load_noaa_ocs_gov)
+        self.toolbar.addAction(noaa_action)
         # ccom.unh.edu
         ccom_action = QtGui.QAction(QtGui.QIcon(os.path.join(self.media, 'ccom.png')), 'ccom.unh.edu', self)
         ccom_action.setShortcut('Alt+C')
@@ -68,6 +81,40 @@ class Info(QtGui.QMainWindow):
         grid.addWidget(self.web, 2, 0)
         frame.setLayout(grid)
 
+        # create an author dialog
+        self.authors_dialog = QtGui.QDialog(self)
+        self.authors_dialog.setWindowTitle("Write to the authors")
+        self.authors_dialog.setMaximumSize(QtCore.QSize(150, 120))
+        self.authors_dialog.setMaximumSize(QtCore.QSize(300, 240))
+        vbox = QtGui.QVBoxLayout()
+        self.authors_dialog.setLayout(vbox)
+
+        hbox = QtGui.QHBoxLayout()
+        vbox.addLayout(hbox)
+        hbox.addStretch()
+        logo = QtGui.QLabel()
+        logo.setPixmap(QtGui.QPixmap(os.path.join(self.media, 'favicon.png')))
+        hbox.addWidget(logo)
+        hbox.addStretch()
+
+        vbox.addSpacing(10)
+
+        text0 = QtGui.QLabel(self.authors_dialog)
+        text0.setOpenExternalLinks(True)
+        vbox.addWidget(text0)
+        text0.setText("""
+        <b>Comments and feature requests:</b><br>
+        Giuseppe Masetti  <a href=\"mailto:gmasetti@ccom.unh.edu?Subject=Hydroffice%20SoundSpeed\">gmasetti@ccom.unh.edu</a><br>
+        Barry Gallagher  <a href=\"mailto:barry.gallagher@noaa.gov?Subject=Hydroffice%20SoundSpeed\">barry.gallagher@noaa.gov</a><br>
+        Brian Calder  <a href=\"mailto:brc@ccom.unh.edu?Subject=Hydroffice%20SoundSpeed\">brc@ccom.unh.edu</a><br>
+        Chen Zhang  <a href=\"mailto:chen.zhang@noaa.gov?Subject=Hydroffice%20SoundSpeed\">chen.zhang@noaa.gov</a><br>
+        Matt Wilson  <a href=\"mailto:matthew.wilson@noaa.gov?Subject=Hydroffice%20SoundSpeed\">matthew.wilson@noaa.gov</a><br>
+        Jack Riley  <a href=\"mailto:jack.riley@noaa.gov?Subject=Hydroffice%20SoundSpeed\">jack.riley@noaa.gov</a><br>
+        <br>
+        <b>Bugs and support:</b><br>
+        <a href=\"mailto:hydroffice.soundspeed@ccom.unh.edu?Subject=Hydroffice%20SoundSpeed\">hydroffice.soundspeed@ccom.unh.edu</a>
+        """)
+
         # load default
         self.load_default()
 
@@ -90,6 +137,11 @@ class Info(QtGui.QMainWindow):
         self.url_input.setText(url)
         self.web.load(QtCore.QUrl(url))
 
+    def load_noaa_ocs_gov(self):
+        url = 'http://www.nauticalcharts.noaa.gov/'
+        self.url_input.setText(url)
+        self.web.load(QtCore.QUrl(url))
+
     def load_ccom_unh_edu(self):
         url = 'http://ccom.unh.edu'
         self.url_input.setText(url)
@@ -99,6 +151,9 @@ class Info(QtGui.QMainWindow):
         url = 'http://www.unh.edu'
         self.url_input.setText(url)
         self.web.load(QtCore.QUrl(url))
+
+    def show_authors(self):
+        self.authors_dialog.show()
 
 
 class UrlInput(QtGui.QLineEdit):
