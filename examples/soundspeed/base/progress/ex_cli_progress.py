93e13e089d66ff488114755b5d0e26f1f39f1505
gmasetti <gmasetti@ccom.unh.edu>
2017-01-31 22:48:49 -0500

    fixed broken example

--- examples/soundspeed/base/progress/ex_cli_progress.py
+++ examples/soundspeed/base/progress/ex_cli_progress.py
@@ -2,15 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import time
 
-# logging settings
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.NOTSET)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)  # change to WARNING to reduce verbosity, DEBUG for high verbosity
-ch_formatter = logging.Formatter('%(levelname)-9s %(name)s.%(funcName)s:%(lineno)d > %(message)s')
-ch.setFormatter(ch_formatter)
-logger.addHandler(ch)
+from hydroffice.soundspeed.logging import test_logging
 
 from hydroffice.soundspeed.base.progress.cli_progress import CliProgress
 
--- examples/soundspeed/base/progress/ex_progress_bar.py
+++ examples/soundspeed/base/progress/ex_progress_bar.py
@@ -3,22 +3,14 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from time import sleep
 from PySide import QtGui
 
-# logging settings
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.NOTSET)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)  # change to WARNING to reduce verbosity, DEBUG for high verbosity
-ch_formatter = logging.Formatter('%(levelname)-9s %(name)s.%(funcName)s:%(lineno)d > %(message)s')
-ch.setFormatter(ch_formatter)
-logger.addHandler(ch)
+from hydroffice.soundspeed.logging import test_logging
 
 from hydroffice.soundspeed.soundspeed import SoundSpeedLibrary
 
 
 def main():
     app = QtGui.QApplication([])
-    lib = SoundSpeedLibrary(qt_progress=QtGui.QProgressDialog)
+    lib = SoundSpeedLibrary()
     lib.progress.start("TEST")
     sleep(0.5)
     lib.progress.update(30)
