ed68c6c2c621fc77e3f6b97d312225fe34be2250
zhang-c <chen.zhang@noaa.gov>
2017-02-02 14:42:29 -0500

    Revert the Sippican class to the previous version before adding XBT txt reader

--- hydroffice/soundspeed/formats/readers/sippican.py
+++ hydroffice/soundspeed/formats/readers/sippican.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import re
 import datetime as dt
 import logging
 
@@ -33,8 +32,6 @@ class Sippican(AbstractTextReader):
         super(Sippican, self).__init__()
         self.desc = "Sippican"
         self._ext.add('edf')
-        self._ext.add('txt')
-        #self._ext.add('bin')
 
         self.is_var_alpha = False
         self.input_salinity = None
@@ -65,17 +62,8 @@ class Sippican(AbstractTextReader):
         self.ssp.append()  # append a new profile
 
         self._read(data_path=data_path)
-        ftype = data_path[-3:].lower()
-        if ftype == 'bin':
-            pass
-            #self._parse_header_bin()
-            #self._parse_body_bin()
-        elif ftype == 'txt':
-            self._parse_header_txt()
-            self._parse_body_txt()
-        else:
-            self._parse_header_edf()
-            self._parse_body_edf()
+        self._parse_header()
+        self._parse_body()
 
         self.fix()
         self.finalize()
@@ -83,13 +71,7 @@ class Sippican(AbstractTextReader):
         logger.debug('*** %s ***: done' % self.driver)
         return True
 
-    def _parse_header(self):  # UNUSED
-        pass
-
-    def _parse_body(self):  # UNUSED
-        pass
-
-    def _parse_header_edf(self):
+    def _parse_header(self):
         """Parsing header: field header, time, latitude, longitude """
         logger.debug('parsing header')
 
@@ -202,7 +184,7 @@ class Sippican(AbstractTextReader):
                 self.more_fields.append('Status')
         self.ssp.cur.init_more(self.more_fields)
 
-    def _parse_body_edf(self):
+    def _parse_body(self):
         logger.debug('parsing body [alpha: %s]' % self.is_var_alpha)
 
         if self.ssp.cur.meta.sensor_type == Dicts.sensor_types["XBT"]:
@@ -348,126 +330,3 @@ class Sippican(AbstractTextReader):
             self.ssp.cur.data.temp[count] = float(fields[self.field_index["Temperature"]])
             self.ssp.cur.data.sal[count] = float(fields[self.field_index["Salinity"]])
             return
-
-    def _parse_header_txt(self):
-        """Parsing header: field header, time, latitude, longitude """
-        logger.debug('parsing header')
-        # body tokens
-        self.tk_depth = 'Depth'
-        self.tk_temp = 'Temperature'
-        self.tk_speed = 'Sound' 
-        date_str = ''
-
-        for line in self.lines:
-            self.samples_offset += 1
-            line = line.strip()
-
-            if line.startswith(self.tk_latitude):  # latitude
-                lat_str = line.split('|')[-1].lstrip().strip()
-                if len(lat_str) > 0:
-                    try:
-                        lat_deg = int(line.split()[-3])
-                        lat_min = float(line.split()[-2])
-                        lat_dir = line.split()[-1]
-                        self.ssp.cur.meta.latitude = lat_deg + lat_min / 60.
-                        if lat_dir == 'S':
-                            self.ssp.cur.meta.latitude *= -1
-                    except ValueError:
-                        logger.warning('issue in casting the latitude at line #%s' % self.samples_offset)
-            elif line.startswith(self.tk_longitude):  # longitude
-                lon_str = line.split('|')[-1].lstrip().strip()
-                if len(lon_str) > 0:
-                    try:
-                        lon_deg = int(line.split()[-3])
-                        lon_min = float(line.split()[-2])
-                        lon_dir = line.split()[-1]
-                        self.ssp.cur.meta.longitude = lon_deg + lon_min / 60.
-                        if lon_dir == 'W':
-                            self.ssp.cur.meta.longitude *= -1
-                    except ValueError:
-                        logger.warning('issue in casting the longitude at line #%s' % self.samples_offset)
-            elif line.startswith('Year'):
-                date_str = '%s%s' % (date_str, line.split()[-1])
-            elif line.startswith('Month'):
-                date_str = '%s %s' % (date_str, line.split()[-1])
-            elif line.startswith('Day'):
-                date_str = '%s %s' % (date_str, line.split()[-1])
-            elif line.startswith('Hour'):
-                date_str = '%s %s' % (date_str, line.split()[-1])
-            elif line.startswith('Minute'):
-                date_str = '%s %s' % (date_str, line.split()[-1])
-                if len(date_str) == 16:
-                    self.ssp.cur.meta.utc_time = dt.datetime.strptime(date_str, '%Y %m %d %H %M')
-                    try:
-                        self.ssp.cur.meta.utc_time = dt.datetime.strptime(date_str, '%Y %m %d %H %M')
-                    except:
-                        logger.warning('issue in casting the time format at line #%s' % self.samples_offset)
-                else:
-                    logger.warning('issue in casting the time format at or before line #%s' % self.samples_offset)
-            elif line.startswith('Ship Name'):
-                self.ssp.cur.meta.vessel = line.split('|')[-1].strip()
-            elif line.startswith('Probe Serial Number'):
-                self.ssp.cur.meta.sn = line.split()[-1]
-            elif line.startswith('Instrument Type'):
-                match = re.search('Sippican (.*?) \(', line)
-                try:
-                    self.ssp.cur.meta.probe_type = Dicts.probe_types[match.group(1)]
-                    self.ssp.cur.meta.sensor_type = self.sensor_dict[self.ssp.cur.meta.probe_type]
-                except (IndexError, KeyError):
-                    self.ssp.cur.meta.probe_type = Dicts.probe_types['Unknown']
-                    self.ssp.cur.meta.sensor_type = Dicts.sensor_types['Unknown']
-                    logger.warning('reverted to unknown probe type at line #%s' % self.samples_offset)
-            elif line.startswith('Depth'): # field headers
-                col = 0  # field column           
-                for field_type in line.split():
-                    self.field_index[field_type] = col
-                    col += 1
-                break
-
-        if not self.ssp.cur.meta.original_path:
-            self.ssp.cur.meta.original_path = self.fid.path
-
-        # initialize data sample fields
-        self.ssp.cur.init_data(len(self.lines) - self.samples_offset)
-        # initialize additional fields
-        # self.ssp.cur.init_more(self.more_fields)
-
-    def _parse_body_txt(self):
-        """Parsing samples: depth, speed, temp"""
-        logger.debug('parsing body')
-
-        count = 0
-        for line in self.lines[self.samples_offset:len(self.lines)]:
-
-            # skip empty lines
-            if len(line.split()) == 0:
-                continue
-
-            data = line.split()
-            # depth
-            try:
-                self.ssp.cur.data.depth[count] = float(data[self.field_index[self.tk_depth]])
-            except ValueError:
-                logger.warning("invalid conversion parsing of line #%s" % (self.samples_offset + count))
-                continue
-            except IndexError:
-                logger.warning("invalid index parsing of line #%s" % (self.samples_offset + count))
-                continue
-
-            # temperature
-            try:
-                self.ssp.cur.data.temp[count] = float(data[self.field_index[self.tk_temp]])
-            except Exception as e:
-                logger.debug("issue in reading pressure and conductivity: %s -> skipping" % e)
-
-            # sound speed
-            try:
-                #self.ssp.cur.data.speed[count] = 1500
-                self.ssp.cur.data.speed[count] = float(data[self.field_index[self.tk_speed]])
-            except Exception as e:
-                logger.debug("issue in reading pressure and conductivity: %s -> skipping" % e)
-
-            count += 1
-
-        self.ssp.cur.data_resize(count)
-
