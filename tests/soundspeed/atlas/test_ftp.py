2deb486b0f1c5fdbfdf932fb8a0f9b28f515a0da
giumas <gmasetti@ccom.unh.edu>
2017-08-09 19:24:41 -0400

    added ftp test

---
+++ tests/soundspeed/atlas/test_ftp.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+
+from hyo.soundspeed.atlas.ftp import Ftp
+from hyo.soundspeed.base import testing
+
+
+class TestFtp(unittest.TestCase):
+
+    def test_init(self):
+        ftp = Ftp(host="ftp.ccom.unh.edu", password="test@gmail.com", show_progress=True, debug_mode=True)
+
+    def test_get_file(self):
+        ftp = Ftp(host="ftp.ccom.unh.edu", password="test@gmail.com", show_progress=True, debug_mode=True)
+        ftp.get_file(file_src="fromccom/hydroffice/Caris_Support_Files_5_5.zip",
+                     file_dst=os.path.join(testing.output_data_folder(), "test.zip"))
+
+
+def suite():
+    s = unittest.TestSuite()
+    s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestFtp))
+    return s
