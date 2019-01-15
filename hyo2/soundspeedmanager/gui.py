c666cc30a42194f1345691ff0d1649b04103d5cd
giumas <giumas@yahoo.it>
2019-01-14 20:12:27 -0500

    modified Qt message handler to ignore js errors

--- hyo2/soundspeedmanager/gui.py
+++ hyo2/soundspeedmanager/gui.py
@@ -11,8 +11,14 @@ logger = logging.getLogger(__name__)
 
 
 def qt_custom_handler(error_type: QtCore.QtMsgType, error_context: QtCore.QMessageLogContext, message: str):
-    logger.info("Qt error: %s [%s] -> %s"
-                % (error_type, error_context, message))
+
+    if error_context.category == "js":
+        logger.debug("ignoring js error")
+        return
+
+    logger.info("Qt error: %s [%s, %s:%s:%d, v.%s] -> %s"
+                % (error_type, error_context.category, error_context.file, error_context.function,
+                   error_context.line, error_context.version, message))
 
     for line in traceback.format_stack():
         logger.debug("- %s" % line.strip())
