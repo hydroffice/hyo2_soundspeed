f2f95d349a104ca88ad3ea4dfe333d3b9fe03514
giumas <giumas@yahoo.it>
2019-02-17 21:53:04 -0500

    minor fixes

--- examples/workground/ex_tx_svp_request_to_sis.py
+++ examples/workground/ex_tx_svp_request_to_sis.py
@@ -14,7 +14,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # We try all of them in the hopes that one works.
-sensors = ["710", "122", "302", "3020", "2040", "124"]
+sensors = ["710", "122", "302", "3020", "2040"]
 for idx, sensor in enumerate(sensors):
     # talker ID, Roger Davis (HMRG) suggested SM based on something KM told him
     smr20_message = '$SMR20,EMX=%s,' % sensor
