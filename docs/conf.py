f8a0d74b904954244a5d4598a749588d7a260618
giumas <giuseppemasetti@gmail.com>
2021-11-27 18:36:11 +0100

    manual updated

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ copyright = u'2021, CCOM/JHC,UNH'
 # The short X.Y version.
 version = '2021.2'
 # The full version, including alpha/beta/rc tags.
-release = '2021.2.5'
+release = '2021.2.6'
 
 numfig = True
 
