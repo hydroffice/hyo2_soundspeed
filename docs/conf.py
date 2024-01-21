3a24961302bb513f61cd001dca41d39e8fbf42e4
giumas <giuseppemasetti@gmail.com>
2024-01-21 16:15:08 +0100

    adopted new theme

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,7 @@ pygments_style = 'colorful'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'hyo2'
+html_theme = 'qc4'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
